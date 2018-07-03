a25389bcfb63096a22b0b10e6cb481cd4cb37b7a
Damiao Rodrigues <adamiaonr@gmail.com>
2018-07-03 17:17:39 +0100

    enhanced cbt.py script (monitor setting code)

--- wifi-assist/src/data-collection/cbt.py
+++ wifi-assist/src/data-collection/cbt.py
@@ -12,22 +12,37 @@ def capture(iface, time, output_file):
     cmd = ["timeout", str(time), "tcpdump", "-i", iface, "-y", "IEEE802_11_RADIO", "-s0", "-w", output_file]
     proc = subprocess.call(cmd)
 
-def set_channel(iface, channel):
-    # ifconfig <iface> down
-    cmd = ["ifconfig", iface, "down"]
-    proc = subprocess.call(cmd)
+def set_channel(iface, channel = 1, bw = 'HT20'):
 
-    # iwconfig <iface> mode monitor
-    cmd = ["iwconfig", iface, "mode", "monitor"]
-    proc = subprocess.call(cmd)
+    # here's the high-level procedure: 
+    #   1) bring <iface> down
+    #   2) set <iface> in monitor mode
+    #   3) bring <iface> up again
+    #   4) set channel to channel nr. or freq. value (in MHz)
+    cmds = []
 
-    # iwconfig <iface> up
-    cmd = ["ifconfig", iface, "up"]
-    proc = subprocess.call(cmd)
+    cmds.append(["ifconfig", iface, "down"])
+    cmds.append(["iwconfig", iface, "mode", "monitor"])
+    cmds.append(["ifconfig", iface, "up"])
 
-    # iwconfig <iface> channel <channel>
-    cmd = ["iwconfig", iface, "channel", str(channel)]
-    proc = subprocess.call(cmd)
+    if (int(channel) <= 6080) and (int(channel) >= 2412):
+        cmds.append(["iw", "dev", iface, "set", "freq", str(channel)])
+    elif (int(channel) <= 216) and ((int(channel) > 0)):
+        cmds.append(["iw", "dev", iface, "set", "channel", str(channel)])
+    else:
+        sys.stderr.write("""%s: [ERROR] invalid channe/freq. argument. aborting.\n""" % sys.argv[0]) 
+        return -1
+        
+    if bw in ['HT20', 'HT40+', 'HT40-']:
+        cmds[-1].append(bw) 
+    else:
+        sys.stderr.write("""%s: [ERROR] invalid channel bw argument. aborting.\n""" % sys.argv[0]) 
+        return -1
+
+    for cmd in cmds:
+        proc = subprocess.call(cmd)
+
+    return 0
 
 if __name__ == "__main__":
 
@@ -54,7 +69,11 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--set-monitor-mode", 
-         help = """set 'iface' to monitor mode on channel x. e.g.: --set-monitor-mode 48""")
+         help = """set 'iface' to monitor mode to channel x and channel bandwidth y.
+syntax is '--set-monitor-mode <x>:<y>'.
+for frequency arguments, use MHz units, e.g. 5240 MHz for channel 48.
+default value is 1:HT20.
+e.g.: '--set-monitor-mode 48:HT40+' or '--set-monitor-mode 5240:HT40+'""")
 
     args = parser.parse_args()
 
@@ -70,7 +89,16 @@ if __name__ == "__main__":
         sys.exit(1)
 
     if args.set_monitor_mode:
-        set_channel(args.iface, args.set_monitor_mode)
+
+        channel = args.set_monitor_mode.split(':')[0]
+        channel_bw = 'HT20'
+        if len(args.set_monitor_mode.split(':')) > 1:
+            channel_bw = args.set_monitor_mode.split(':')[1]
+        
+        if (set_channel(args.iface, channel, channel_bw) < 0):
+            sys.stderr.write("""%s: [ERROR] error while setting monitor mode. aborting.\n""" % sys.argv[0])
+            sys.exit(1)
+
         sys.exit(0)
 
     if not args.output_dir:
