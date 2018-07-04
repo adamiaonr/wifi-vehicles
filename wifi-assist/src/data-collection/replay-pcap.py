b77a71704b3293120051d08165f0b37d7b493f62
Damiao Rodrigues <adamiaonr@gmail.com>
2018-07-04 14:31:22 +0100

    refurbished replay script

--- wifi-assist/src/data-collection/replay-pcap.py
+++ wifi-assist/src/data-collection/replay-pcap.py
@@ -8,7 +8,7 @@ import subprocess
 import sys
 
 from scapy.all import *
-from scapy.utils import rdpcap
+from scapy.utils import rdpcap, wrpcap
 
 if __name__ == "__main__":
 
@@ -36,12 +36,20 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(1)
 
-    pkts = rdpcap(args.capture, 50)
+    # read x packets from .pcap file
+    pkts = rdpcap(args.capture, 52)
+
+    # replay wlan data frames (type = 0x02, subtype = 0x08)
     for pkt in pkts:
         if pkt.haslayer(Dot11):
             if (pkt.type == 0x02) and (pkt.subtype == 0x08):
-                pkt[Dot11].addr1 = 'ff:ff:ff:ff:ff'
-                pkt[Dot11].addr3 = 'ff:ff:ff:ff:ff'
-                pkt[IP].dst = '255.255.255.255'
-                pkt.show()
-                # sendp(pkt, iface = args.iface, inter = 0.1, loop = 1)
+
+                # remove IP payload, so that frames fit sendp() max. allowed length
+                pkt[IP].remove_payload()
+                # set IP datagram len to 20 byte (header only)
+                pkt[IP].len = 20
+                # remove IP header checksum to force re-calculation
+                del pkt[IP].chksum
+                pkt[IP].show2()
+                # send the wlan frame away
+                sendp(pkt, iface = args.iface)
