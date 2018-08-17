d42d91bbea37183c9c9ad7d32a4586d1327b7368
adamiaonr <adamiaonr@gmail.com>
2018-08-17 15:12:20 +0100

    improved scripts for unifi ac lite ap (w/ tcpdump sent to nfs folder now)

--- wifi-assist/src/data-collection/get-cbt.sh
+++ wifi-assist/src/data-collection/get-cbt.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-if [ $# -lt 1 ]
+if [ $# -lt 2 ]
 then
-    echo "usage : $0 <trace-nr>"
+    echo "usage : $0 <trace-nr> <output-dir>"
     exit 1
 fi
 
 trace_nr=$1
+output_dir=$2
 
 echo $$ > /var/run/get-cbt.pid
 
@@ -32,12 +33,18 @@ else
 	exit 1
 fi
 
-mac_addr=$(iw $wiface info | awk '/addr/ {print $2}')
-# prefix to prepend to logger output:
-#   - mac_addr (remove ':')
-#   - trace_nr
-#   - log line type : 'cbt' or 'iperf'
-logger_prefix="$(echo $mac_addr | sed -r 's/[:]//g')|$trace_nr"
+# mac_addr=$(iw $wiface info | awk '/addr/ {print $2}')
+# # prefix to prepend to logger output:
+# #   - mac_addr (remove ':')
+# #   - trace_nr
+# #   - log line type : 'cbt' or 'iperf'
+# logger_prefix="$(echo $mac_addr | sed -r 's/[:]//g')|$trace_nr"
+
+# create cbt.csv file
+output_file="$output_dir"/"cbt.$(date +"%s").csv"
+touch $output_file
+# add header
+echo -e "timestamp,freq,noise,cat,cbt,crt,ctt" > $output_file
 
 while [ "$stop_loop" = false ]; do
 
@@ -46,14 +53,15 @@ while [ "$stop_loop" = false ]; do
 
 	# extract cbt survey components
 	freq=$(echo "$survey" | awk '/frequency/ {print $2}')
-	noise=$(echo "$survey" | awk '/noise/ {print $2}')
+	noiz=$(echo "$survey" | awk '/noise/ {print $2}')
 	cat=$(echo "$survey" | awk '/channel active time/ {print $4}')
 	cbt=$(echo "$survey" | awk '/channel busy time/ {print $4}')
 	crt=$(echo "$survey" | awk '/channel receive time/ {print $4}')
 	ctt=$(echo "$survey" | awk '/channel transmit time/ {print $4}')
 
-	# send csv line to rsyslog
-	echo "$freq,$noise,$cat,$cbt,$crt,$ctt" |  logger -t "$logger_prefix|cbt"
+	# send csv line to rsyslog (headed by a timestamp)
+	echo -e "$(date +"%s"),$freq,$noiz,$cat,$cbt,$crt,$ctt" >> $output_file
+	# echo "$(date +"%s"),$freq,$noiz,$cat,$cbt,$crt,$ctt" |  logger -t "$logger_prefix|cbt"
 
 	sleep 1
 
--- wifi-assist/src/data-collection/iperf3-to-mobile.openwrt.sh
+++ wifi-assist/src/data-collection/iperf3-to-mobile.openwrt.sh
@@ -2,9 +2,9 @@
 
 # usage: ifperf3-to-mobile.openwrt.sh <bitrate> <server-ip> <server-port> <iface> <remote-logger-ip>
 
-if [ $# -lt 4 ]
+if [ $# -lt 5 ]
 then
-    echo "usage : $0 <protocol> <trace-nr> <server_ip> <server_port> (if protocol = 'udp': <udp-bitrate>)"
+    echo "usage : $0 <protocol> <trace-nr> <output-dir> <server_ip> <server_port> (if protocol = 'udp': <udp-bitrate>) (optional: <capture>)"
     exit 1
 fi
 
@@ -13,32 +13,16 @@ echo $$ > /var/run/iperf3-to-mobile.pid
 protocol=$1
 trace_nr=$2
 # channel=$3
-server_ip=$3
-server_port=$4
+output_dir=$3
+server_ip=$4
+server_port=$5
 
 echo "started iperf3 script w/ params:"
 echo "  protocol: $protocol"
-echo "  ip:port (iperf3 server): $server_ip:$server_port"
-# echo "  channel (iperf3 server): $server_ip:$server_port"
 echo "  trace-nr: $trace_nr"
-
-if [ "$protocol" == "udp" ]
-then
-
-    if [ $# -lt 5 ]
-    then
-        echo "usage : $0 <protocol> <trace-nr> <server_ip> <server_port> <udp-bitrate>"
-        exit 1
-    fi
-
-    bitrate=$5
-    echo '  bitrate: "$bitrate" M (bps)'
-
-elif [ "$protocol" != "tcp" ]
-then  
-    echo "error : unknown protocol : $protocol. aborting."
-    exit 1
-fi
+echo "  output-dir: $output_dir"
+echo "  ip:port: $server_ip:$server_port"
+# echo "  channel (iperf3 server): $server_ip:$server_port"
 
 # trap ctrl-c and call signal_handler()
 stop_loop=false
@@ -50,7 +34,7 @@ signal_handler() {
     stop_loop=true
 }
 
-# get mac addr of wifi iface
+# get wifi iface name
 wiface=""
 if [ "$(iwinfo wlan0 info | awk '/Access Point/ {print $3}')" == "24:05:0F:61:51:14" ]
 then
@@ -63,19 +47,57 @@ else
     exit 1
 fi
 
-mac_addr=$(iw $wiface info | awk '/addr/ {print $2}')
-# prefix to prepend to logger output:
-#   - mac_addr (remove ':')
-#   - trace_nr
-#   - protocol : 0 for udp, 1 for tcp
-#   - log line type : 'cbt' or 'iperf'
-logger_prefix="$(echo $mac_addr | sed -r 's/[:]//g')|$trace_nr"
 if [ "$protocol" == "udp" ]
 then
-    logger_prefix="$logger_prefix|0|iperf"
+
+    if [ $# -lt 5 ]
+    then
+        echo "usage : $0 <protocol> <trace-nr> <output-dir> <server_ip> <server_port> <udp-bitrate>"
+        exit 1
+    fi
+
+    bitrate="$6""M"
+    echo "  bitrate: $bitrate (bps)"
+
+    if [ "$7" ]
+    then
+        echo "  capture: yes"
+        tcpdump -i "$wiface" -s0 udp -w "$output_dir"/"iperf3-to-mobile.capture.$(date +"%s").pcap" &
+    fi
+
+elif [ "$protocol" == "tcp" ]
+then  
+
+    if [ "$6" ]
+    then
+        echo "  capture: yes"
+        tcpdump -i "$wiface" -s0 tcp -w "$output_dir"/"iperf3-to-mobile.capture.$(date +"%s").pcap" &
+    fi
+
 else
-    logger_prefix="$logger_prefix|1|iperf"
+    echo "error : unknown protocol : $protocol. aborting."
+    exit 1
 fi
+
+# mac_addr=$(iw $wiface info | awk '/addr/ {print $2}')
+# # prefix to prepend to logger output:
+# #   - mac_addr (remove ':')
+# #   - trace_nr
+# #   - protocol : 0 for udp, 1 for tcp
+# #   - log line type : 'cbt' or 'iperf'
+# logger_prefix="$(echo $mac_addr | sed -r 's/[:]//g')|$trace_nr"
+# if [ "$protocol" == "udp" ]
+# then
+#     logger_prefix="$logger_prefix|0|iperf"
+# else
+#     logger_prefix="$logger_prefix|1|iperf"
+# fi
+
+# create .json file
+output_file="$output_dir"/"iperf3-to-mobile.report.$(date +"%s").json"
+touch $output_file
+# clear .json file
+echo -e "" > $output_file
 
 # run iperf3 in captures of 5 seconds (pipe output to logger)
 while [ "$stop_loop" = false ]; do
@@ -83,9 +105,9 @@ while [ "$stop_loop" = false ]; do
     # extract the output of iperf3
     if [ "$protocol" == "udp" ]
     then
-        output=$(iperf3 -V -J -t 5 -c $server_ip -p $server_port -u -b "$bitrate"M --get-server-output)
+        output=$(iperf3 -V -J -t 5 -c $server_ip -p $server_port -u -b $bitrate --get-server-output)
     else
-        output=$(iperf3 -V -J -t 5 -c $server_ip -p $server_port)
+        output=$(iperf3 -V -J -t 10 -c $server_ip -p $server_port)
     fi
 
     # check for errors in iperf3's output: if errors exist, exit
@@ -95,12 +117,19 @@ while [ "$stop_loop" = false ]; do
         sleep 1
     else
         # otherwise, keep relaying the output to rsyslog
-        echo "$output" | logger -t "$logger_prefix"
+        # printf %s "$output" | logger -t "$logger_prefix"
+        echo -e "$output" >> $output_file
+        # # special purpose while loop for server stats
+        # server_output=$(printf %s "$output" | awk '/server_output_text/ {print}')
+        # if [ "$server_output" != "" ]
+        # then
+        #     echo -e "$server_output" | while read -r line
+        #     do
+        #         # printf %s "$line" | logger -t "$logger_prefix"
+        #         echo %s "$line" | logger -t "$logger_prefix"
+        #     done
+        # fi
     fi
 done
-
-# kill tcpdump after CTRL+C
-# killall tcpdump
-# killall logread
 
 exit
