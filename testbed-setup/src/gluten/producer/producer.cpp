64c04fdf2aab9296f0d4f165ff5ca096ab279ad1
Damiao Rodrigues <adamiaonr@gmail.com>
2019-08-15 14:33:41 +0100

    changes to producer app

--- testbed-setup/src/gluten/producer/producer.cpp
+++ testbed-setup/src/gluten/producer/producer.cpp
@@ -1,15 +1,7 @@
-/* echo-client-udp.c */
-
-/* Simple UDP echo client - tries to send everything read from stdin
-   as a single datagram (MAX 1MB)*/
-
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <fstream>  // reading .nw files
+/* producer.c */
 
 #include <stdio.h>      /* standard C i/o facilities */
-#include <stdlib.h>     /* needed for atoi() */
+#include <stdlib.h>     /* needed for atoi() and atof() */
 #include <unistd.h>     /* defines STDIN_FILENO, system calls,etc */
 #include <sys/types.h>  /* system data type definitions */
 #include <sys/socket.h> /* socket specific definitions */
@@ -21,13 +13,6 @@
 #include <signal.h>
 #include <unistd.h>
 #include <errno.h>
-
-struct echo_record {
-    struct timeval timestamp;
-};
-
-#define PACKET_SIZE (unsigned int) ((16 + 20 + 8) + sizeof(struct echo_record))
-#define MAXBUF 10*1024
 
 static volatile int carry_on = 1;
 
@@ -45,133 +30,77 @@ int main(int argc, char **argv) {
     int n_sent;
     int n_read;
 
-    if (argc != 5) {
-        std::cerr << "echo_client::main() : [ERROR] usage : " << argv[0] << " <server-ip> <port-number> <bps> <output-dir>" << std::endl;
+    if (argc != 4) {
+        fprintf(stderr, "[ERROR] usage : %s <consumer-ip> <consumer-port> <bps>\n", argv[0]);
         exit(1);
     }
 
     // set handler for SIGINT (CNTRL+C)
     signal(SIGINT, handler);
 
-    // // files w/ seq. packet seq. numbers
-    // std::vector<std::ofstream> files = std::vector<std::ofstream> (2);
-    // std::string filenames[2] = {"sent.tsv", "rcvd.tsv"};
-    // for (unsigned int i = 0; i < files.size(); i++) {
-    //     files[i].open(std::string(argv[4]) + std::string("/") + filenames[i]);
-    //     files[i] << "timestamp\n";
-    // }
-
     // create udp socket
     if ((sk = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-        std::cerr << "echo_client::main() : [ERROR] problem creating socket" << std::endl;
+        perror("[ERROR] problem creating socket");
         exit(1);
     }
-
 
     // get address of server from hostname passed as arg
     // FIXME: does gethostbyname() work w/ ip addresses? shouldn't it be getaddrinfo()?
     server.sin_family = AF_INET;
     if ((hp = gethostbyname(argv[1])) == 0) {
-        std::cerr << "echo_client::main() : [ERROR] couldn't get host for " << argv[1] << std::endl;
+        fprintf(stderr, "[ERROR] couldn't get host for %s\n", argv[1]);
         exit(1);
     }
     
-    // copy address got by gethostname() into sockaddr_in struct    
     memcpy(&server.sin_addr.s_addr, hp->h_addr, hp->h_length);
     // set port in sockaddr_in struct
     server.sin_port = htons(atoi(argv[2]));
-
-    // send udp packet w/ timestamp as payload
-    // struct echo_record * payload = (struct echo_record *) buf;
+    // empty payload to send over udp
     unsigned char payload[1472];
 
-    // set a recv timeout on sk
-    struct timeval read_timeout, s, e;
-    read_timeout.tv_sec = 1;
-    read_timeout.tv_usec = 0;
-    setsockopt(sk, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
-    // # of bytes sent
+    // // set a 1 sec recv timeout on send socket
+    // FIXME : this would only 
+    // struct timeval read_timeout, s, e;
+    // read_timeout.tv_sec = 1;
+    // read_timeout.tv_usec = 0;
+    // setsockopt(sk, SOL_SOCKET, SO_RCVTIMEO, &read_timeout, sizeof read_timeout);
+    // // set broadcast mode on send socket
+    // int broadcast = 1;
+    // setsockopt(sk, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+
     unsigned long int bytes_sent = 0;
     double interval = (((double) (PACKET_SIZE * 8.0)) / ((double) std::stof(argv[3]))) * 1000000.0;
-    std::cout << "echo_client::main() : [INFO] sleep interval for bw " << std::stof(argv[3]) << " bps : " << interval << " us" << std::endl;
+    fprintf(stdout, "[INFO] sleep interval for bw %.3f bps : %.3f us\n", atof(argv[3]), interval);
 
-    unsigned long counter = 0;
+    unsigned long byte_cntr = 0, pckt_cntr = 0;
     time_t init_time, curr_time;
+
     init_time = time(NULL);
-
-    gettimeofday(&s, NULL);
     while (carry_on) {
-
         curr_time = time(NULL);
         if (curr_time > init_time) {
-
-            std::cout << "[INFO] sent " << counter << " bytes (" << (double) (counter * 8.0) / (double) ((curr_time - init_time) * 1000000.0) << " Mbps)" << std::endl;
-            counter = 0;
+            double bitrate = (double) (byte_cntr * 8.0) / (double) ((curr_time - init_time) * 1000000.0);
+            fprintf(stdout, "%d,%d,%d,%d,%.3f\n", curr_time, pckt_cntr, byte_cntr, elapsed_time, bitrate);
+            
+            byte_cntr = 0;
+            pckt_cntr = 0;
             init_time = curr_time;
         }
 
-        // gettimeofday(&(payload->timestamp), NULL);
-        // buf_len = sizeof(struct echo_record);
         n_sent = sendto(sk, payload, 1472, MSG_DONTWAIT, (struct sockaddr*) &server, sizeof(server));
-        // std::cout << "echo_client::main() : [INFO] sent : " << (int) payload->timestamp.tv_sec << "." << (int) payload->timestamp.tv_usec << std::endl;
 
         // check for problems in send()
         if ((n_sent < 0) && (init_time != curr_time)) {
-            // std::cerr << "echo_client::main() : [ERROR] sendto() returned < 0" << std::endl;
-            perror("echo_client::main() : [ERROR] sendto() returned < 0");
+            perror("[ERROR] sendto() returned < 0");
         }
 
         if (n_sent > 0) {
             counter += n_sent;
+            pckt_cntr++;
         }
 
-        // // save timestamp on 'sent' file
-        // files[0] << payload->timestamp.tv_sec << "." << payload->timestamp.tv_usec << "\n";
-
-        // // block till the echo response is given
-        // n_read = recvfrom(sk, buf, MAXBUF, 0, NULL, NULL);
-        // if (n_read < 0) {
-
-        //     int errsv = errno;
-        //     if (!((errsv == EWOULDBLOCK) || (errsv == EAGAIN))) {
-        //         std::cerr << "echo_client::main() : [ERROR] recvfrom() returned < 0" << std::endl;
-        //         exit(1);
-        //     }
-        // }
-
-        // // extract payload from response & save it to file
-        // struct echo_record * echo_response = (struct echo_record *) buf;
-        // // save timestamp on 'sent' file
-        // files[1] << echo_response->timestamp.tv_sec << "." << echo_response->timestamp.tv_usec << "\n";
-        // // std::cout << "echo_client::main() : [INFO] got : " << (int) echo_response->timestamp.tv_sec << "." << (int) echo_response->timestamp.tv_usec << std::endl;
-
-        // FIXME: this should be adjustable by an arg
-        usleep(0);
-
-        // // FIXME: overflow problems ahead?
-        // bytes_sent += (((16 + 20 + 8)) + (sizeof(struct echo_record)));
+        usleep(interval);
     }
-
-    // gettimeofday(&e, NULL);
-
-    // std::cout << "echo_client::main() : [INFO] payload size : " << (sizeof(struct echo_record) * 8) << " bit (" << sizeof(struct echo_record) << " byte)" << std::endl;
-    // std::cout << "echo_client::main() : [INFO] packet size : " << (PACKET_SIZE * 8) << " bit (" << PACKET_SIZE << " byte)" << std::endl;
-    // double time_elapsed = (e.tv_sec - s.tv_sec) + ((e.tv_usec - s.tv_usec) / 1000000.0);
-    // std::cout << "echo_client::main() : [INFO] time elapsed : " << time_elapsed << " sec" << std::endl;
-    // std::cout << "echo_client::main() : [INFO] avg. sleep time : " << time_elapsed / ((double) bytes_sent / (double) PACKET_SIZE) << " sec" << std::endl;
-    // std::cout << "echo_client::main() : [INFO] avg. bit rate : " 
-    //     << (double) (bytes_sent * 8) / (time_elapsed) << " bps (" 
-    //     << (double) (bytes_sent) / (time_elapsed) << " B(byte) ps)" << std::endl;
-
-    // files[0].close();
-    // files[1].close();
-
-    // // add report file
-    // std::ofstream report;
-    // report.open(std::string(argv[4]) + std::string("/report.tsv"));
-    // report << "payload\tpacket\ttime elapsed\tavg sleep time\tavg bitrate\n";
-    // report << (sizeof(struct echo_record) * 8) << "\t" << (PACKET_SIZE * 8) << "\t" << time_elapsed << "\t" << time_elapsed / ((double) bytes_sent / (double) PACKET_SIZE) << "\t" << (double) (bytes_sent * 8) / (time_elapsed) << "\n";
-    // report.close();
 
     exit(0);
 }
